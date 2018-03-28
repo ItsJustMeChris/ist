5c5d0308da40ca5486565e1ca57d85343a9f2911
ItsJustMeChris <dribbleribbletribble@gmail.com>
2018-03-27 21:03:29 -0400

    Push
    Added jQuery Task List

---
+++ jquerytasklist/assets/js/main.js
@@ -0,0 +1,16 @@
+$("#saveButton").click(function() {
+    let div = $('<div></div>', {
+        class: 'delete',
+        id: taskID,
+        text: $("#taskContent").val()
+    }).appendTo($(".tasks")[0]);
+
+    let checkbox = $('<input />', {
+        type: 'checkbox'
+    }).appendTo(div);
+
+    checkbox.click(function() {
+        this.parentElement.remove()
+        currentTasks.splice(this.parentElement.id, 1)
+    })
+})
